70bce73870ed0fea44d750bf4087d1d5d79ceb2b
fontaine618 <simfont@umich.edu>
2023-09-04 18:39:11 -0400

    experiments setup

---
+++ experiment_re/run.R
@@ -0,0 +1,113 @@
+library(batchtools)
+library(data.table)
+library(tidyverse)
+library(magrittr)
+
+# ==============================================================================
+# Setup batchtools registry
+DIR = "./experiment_re/"
+if(!dir.exists(DIR)) dir.create(DIR, recursive=T)
+registry = makeExperimentRegistry(
+  file.dir=paste0(DIR, "registry/"),
+  seed=1,
+  packages=c("dplyr", "magrittr", "LSVCMM", "spfda")
+)
+# ------------------------------------------------------------------------------
+
+
+
+
+# ==============================================================================
+# Setup problem
+synthetic = function(data, job, ...){
+  instance = LSVCMM::generate_synthetic_data(...)
+  return(instance)
+}
+
+addProblem(
+  name="synthetic",
+  fun=synthetic,
+  data=NULL
+)
+
+# for debugging
+instance = synthetic(NULL, NULL)
+# ------------------------------------------------------------------------------
+
+
+
+
+# ==============================================================================
+# Setup algorithms
+source("./algorithms/lsvcmm.R")
+source("./algorithms/spfda.R")
+addAlgorithm(
+  name="LSVCMM",
+  fun=lsvcmm_wrapper
+)
+addAlgorithm(
+  name="LSVCMM.Independent",
+  fun=lsvcmm_wrapper
+)
+addAlgorithm(
+  name="LSVCMM.Cross-sectional",
+  fun=lsvcmm_wrapper
+)
+addAlgorithm(
+  name="SPFDA",
+  fun=spfda_wrapper
+)
+# ------------------------------------------------------------------------------
+
+
+
+
+# ==============================================================================
+# Experimental design
+n_reps=100
+problems = list(
+  `synthetic`=CJ(
+    seed=seq(n_reps),
+    observation_variance=1.,
+    random_effect_ar1_correlation=1.,
+    random_effect_variance_ratio=c(0., 0.2, 0.5, 1., 1.5, 2., 4.),
+    prop_observed=1.
+  )
+)
+
+algorithms = list(
+  `LSVCMM`=data.table(cross_sectional=F, independent=F, penalty.adaptive=1.),
+  `LSVCMM.Independent`=data.table(cross_sectional=F, independent=T, penalty.adaptive=1.),
+  `LSVCMM.Cross-sectional`=data.table(cross_sectional=T, independent=T, penalty.adaptive=1.),
+  `SPFDA`=data.table()
+)
+
+addExperiments(
+  prob.designs=problems,
+  algo.designs=algorithms,
+  repls=1
+)
+# ------------------------------------------------------------------------------
+
+
+
+
+# ==============================================================================
+# Run
+summarizeExperiments()
+getStatus()
+
+resources = list(
+  account="stats_dept1",
+  partition="standard",
+  memory="6g", # this is per cpu
+  ncpus=1,
+  walltime="60:00",
+  chunks.as.arrayjobs=FALSE,
+  job_name="LSVCMM_re"
+)
+
+chunk_df = data.table(job.id=1:2800, chunk=1:100)
+head(chunk_df)
+submitJobs(chunk_df, resources)
+# ------------------------------------------------------------------------------
